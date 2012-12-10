c09661b6840a9374b2d01c5340ae08ebf0182504
gaoweiwei <getfirstblood@gmail.com>
2012-12-10 20:28:00 +0800

    Initial Commit

---
+++ decorate pattern/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  decorate pattern
+//
+//  Created by gaoweiwei on 12-12-10.
+//  Copyright (c) 2012年 gaoweiwei. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
